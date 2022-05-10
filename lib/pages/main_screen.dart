af43ccacd0e6d46c608d5b3db62e98314ec0be2e
vitalik98 <vitalii.mazanik.98@gmail.com>
2022-05-10 13:09:07 +0300

    Added main screen with tabs for future pages implementation

---
+++ lib/pages/main_screen.dart
@@ -0,0 +1,39 @@
+import 'package:eventor/pages/profile.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  //add screen widget to list for tabs (mind order)
+  List<Widget> pageList = <Widget>[
+    const Scaffold(),
+    const Scaffold(),
+    const ProfilePage()
+  ];
+  int _selectedIndex = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pageList[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: (value) {
+          setState(() {
+            _selectedIndex = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: "Events"),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: "Map"),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
